463ecc5496eb51060777c7cbb3c72cc086159a9c
Amarachi Ege-Obetta <amy1234obetta@gmail.com>
2026-08-28 02:15:18 +0100

    fix: provide test database fallback for CI

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL","sqlite+aiosqlite:///./test.db")
 
 engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
